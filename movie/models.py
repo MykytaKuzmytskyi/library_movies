ef20774fd890c617ec0a22a85412d46d8495ca75
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-02-27 20:12:18 +0200

    Created models Director, Movie Actor.

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+        unique_together = ('first_name', 'last_name',)
+        ordering = ("first_name", )
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Director(Person):
+    pass
+
+
+class Actor(Person):
+    pass
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=2550)
+    year = models.PositiveIntegerField()
+    directors = models.ManyToManyField(Director)
+    actors = models.ManyToManyField(Actor)
+
+    def __str__(self):
+        return f"{self.title} - ({self.year})"
+
+    class Meta:
+        unique_together = ('title', 'year',)
