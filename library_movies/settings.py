9b88e1d1fdd4d0b8e8f7ca0b3f8c568f86dcf453
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-02-28 08:13:46 +0200

    Modified DEBUG.

--- library_movies/settings.py
+++ library_movies/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+debug_value = os.environ.get("DEBUG", "False")
+DEBUG = bool(debug_value.lower() == "true")
 
 ALLOWED_HOSTS = ["127.0.0.1", "library-movies.onrender.com"]
 
